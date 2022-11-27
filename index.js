750ed2844b7e31eda1184e411c143aa7e7069b0b
Joao Victor <j17duarte@hotmail.com>
2022-11-26 23:48:42 -0300

    fix: move express config to index

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-const app = require('./config/server');
 const routes = require('./app/routes/routes');
-require('./startup/prod')(app);
+
+const express = require('express');
+const expressSession = require('express-session');
+const body_parser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5080;
+
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+app.use(express.static('./public'));
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(body_parser.urlencoded({ extended:true }));
+
+app.use(expressSession({
+	secret: 'any',
+	name: 'uniqueSessionID',
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		maxAge: 10000 * 60,
+		secure: false,
+		httpOnly: true,
+	}
+}))
+
+if (process.env.ENVIRONMENT === 'DEVL') {
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+  })
+}
 
 //GET
 routes.home(app);
